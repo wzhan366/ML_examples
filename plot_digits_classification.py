3a242b4659139b24a94ba37a3045fafb2dcb02eb
Ian <wzhan366@uwo.ca>
2016-11-03 14:40:56 -0400

    Add files via upload
    Analysis the samples on scikit-learn

---
+++ plot_digits_classification.py
@@ -0,0 +1,79 @@
+"""
+================================
+Recognizing hand-written digits
+================================
+
+An example showing how the scikit-learn can be used to recognize images of
+hand-written digits.
+
+This example is commented in the
+:ref:`tutorial section of the user manual <introduction>`.
+
+"""
+# print __doc__ 
+
+# Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
+# License: BSD 3 clause
+
+'''My ideas of this example
+1. this one didn't use cross validation method
+2. gamma and C should be selected systematicly
+	- VC dimension should be used
+3. should try more than one model
+4. the input should use more reasonable features instead of using the whole pixles..
+'''
+
+
+# Standard scientific Python imports
+import matplotlib.pyplot as plt
+
+# Import datasets, classifiers and performance metrics
+from sklearn import datasets, svm, metrics
+
+# The digits dataset
+digits = datasets.load_digits()
+
+
+# The data that we are interested in is made of 8x8 images of digits, let's
+# have a look at the first 4 images, stored in the `images` attribute of the
+# dataset.  If we were working from image files, we could load them using
+# matplotlib.pyplot.imread.  Note that each image must have the same size. For these
+# images, we know which digit they represent: it is given in the 'target' of
+# the dataset.
+images_and_labels = list(zip(digits.images, digits.target))
+
+
+# for index, (image, label) in enumerate(images_and_labels[:4]):
+#     plt.subplot(2, 4, index + 1)
+#     plt.axis('off')
+#     plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+#     plt.title('Training: %i' % label)
+
+# To apply a classifier on this data, we need to flatten the image, to
+# # turn the data in a (samples, feature) matrix:
+n_samples = len(digits.images)
+data = digits.images.reshape((n_samples, -1)) #basiclly turn the 2D matrix to a 1D array and feed this to SVM
+
+
+# Create a classifier: a support vector classifier
+classifier = svm.SVC(gamma=0.001) #a Random gamma....which should be selected more sysmaticly
+
+# We learn the digits on the first half of the digits
+classifier.fit(data[:n_samples / 2], digits.target[:n_samples / 2]) # use half of data to train is not a good idea, vc analyse should be consider
+
+# Now predict the value of the digit on the second half:
+expected = digits.target[n_samples / 2:]
+predicted = classifier.predict(data[n_samples / 2:])
+
+print "Classification report for classifier %s:\n%s\n" \
+      % (classifier, metrics.classification_report(expected, predicted))
+print "Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted)
+
+# images_and_predictions = list(zip(digits.images[n_samples / 2:], predicted))
+# for index, (image, prediction) in enumerate(images_and_predictions[:4]):
+#     plt.subplot(2, 4, index + 5)
+#     plt.axis('off')
+#     plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+#     plt.title('Prediction: %i' % prediction)
+
+# plt.show()
