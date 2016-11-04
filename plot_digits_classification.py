a7f5d22b48b4bba864e727e375b131450fae5045
Ian <wzhan366@uwo.ca>
2016-11-04 10:19:56 -0400

    Update plot_digits_classification.py

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -1,64 +1,150 @@
+"""
+This one use the unsupervised learning method PCA first,
+then use supervised learning method SVM with the parameter search method GridSearch
 
+Highlight:
+1. gridsearch method for svm
+2. using pca to decrease the input of svm
 
-'''My ideas of this example
-1. this one didn't use cross validation method
-2. gamma and C should be selected systematicly
-	- VC dimension should be used
-3. should try more than one model
-4. the input should use more reasonable features instead of using the whole pixles..
-'''
+Might be better:
+Use PCA made the input of SVM decreased which will boost the system performance
 
+If we can use cross validation method on top of PCA, it will give us a better face eig_vectors
 
-# Standard scientific Python imports
+"""
+from __future__ import print_function
+
+from time import time
+import logging
 import matplotlib.pyplot as plt
 
-# Import datasets, classifiers and performance metrics
-from sklearn import datasets, svm, metrics
+import pdb
 
-# The digits dataset
-digits = datasets.load_digits()
-
-
-# The data that we are interested in is made of 8x8 images of digits, let's
-# have a look at the first 4 images, stored in the `images` attribute of the
-# dataset.  If we were working from image files, we could load them using
-# matplotlib.pyplot.imread.  Note that each image must have the same size. For these
-# images, we know which digit they represent: it is given in the 'target' of
-# the dataset.
-images_and_labels = list(zip(digits.images, digits.target))
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.datasets import fetch_lfw_people 
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
 
 
-# for index, (image, label) in enumerate(images_and_labels[:4]):
-#     plt.subplot(2, 4, index + 1)
-#     plt.axis('off')
-#     plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-#     plt.title('Training: %i' % label)
+# print(__doc__)
 
-# To apply a classifier on this data, we need to flatten the image, to
-# # turn the data in a (samples, feature) matrix:
-n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1)) #basiclly turn the 2D matrix to a 1D array and feed this to SVM
+# Display progress logs on stdout
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
 
-# Create a classifier: a support vector classifier
-classifier = svm.SVC(gamma=0.001) #a Random gamma....which should be selected more sysmaticly
+###############################################################################
+# Download the data, if not already on disk and load it as numpy arrays
 
-# We learn the digits on the first half of the digits
-classifier.fit(data[:n_samples / 2], digits.target[:n_samples / 2]) # use half of data to train is not a good idea, vc analyse should be consider
+lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4) # this return the pic size of 50 x 37 pixels
+# introspect the images arrays to find the shapes (for plotting)
+n_samples, h, w = lfw_people.images.shape
+print(n_samples, h, w)
+# for machine learning we use the 2 data directly (as relative pixel
+# positions info is ignored by this model) t
+X = lfw_people.data #dataset.data : numpy array of shape (13233, 2914) 
+n_features = X.shape[1]
 
-# Now predict the value of the digit on the second half:
-expected = digits.target[n_samples / 2:]
-predicted = classifier.predict(data[n_samples / 2:])
+# the label to predict is the id of the person
+y = lfw_people.target #this one is the name assoiated with pics
+target_names = lfw_people.target_names
+print(target_names[:10], target_names.shape)
+n_classes = target_names.shape[0]
 
-print "Classification report for classifier %s:\n%s\n" \
-      % (classifier, metrics.classification_report(expected, predicted))
-print "Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted)
+print("Total dataset size:")
+print("n_samples: %d" % n_samples)
+print("n_features: %d" % n_features)
+print("n_classes: %d" % n_classes)
 
-# images_and_predictions = list(zip(digits.images[n_samples / 2:], predicted))
-# for index, (image, prediction) in enumerate(images_and_predictions[:4]):
-#     plt.subplot(2, 4, index + 5)
-#     plt.axis('off')
-#     plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-#     plt.title('Prediction: %i' % prediction)
 
-# plt.show()
+###############################################################################
+# Split into a training set and a test set using a stratified k fold
+
+# split into a training and testing set
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=42)
+
+
+###############################################################################
+# Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
+# dataset): unsupervised feature extraction / dimensionality reduction
+n_components = 200
+
+print("Extracting the top %d eigenfaces from %d faces"
+      % (n_components, X_train.shape[0]))
+t0 = time()
+pca = PCA(n_components=n_components, svd_solver='randomized',
+          whiten=True).fit(X_train)
+print("done in %0.3fs" % (time() - t0))
+
+
+eigenfaces = pca.components_.reshape((n_components, h, w)) # eigenfaces.shape -> (200, 50, 37)
+
+print("Projecting the input data on the eigenfaces orthonormal basis")
+t0 = time()
+X_train_pca = pca.transform(X_train) # X_train.shape ->(966, 1850), X_train_pca -> (966,200)
+
+X_test_pca = pca.transform(X_test)
+print("done in %0.3fs" % (time() - t0))
+
+
+###############################################################################
+# Train a SVM classification model
+
+print("Fitting the classifier to the training set")
+t0 = time()
+param_grid = {'C': [1e3, 5e3, 1e4, 5e4, 1e5],
+              'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1], }
+clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
+clf = clf.fit(X_train_pca, y_train) # y_train is belong to set([0, 1, 2, 3, 4, 5, 6])
+print("done in %0.3fs" % (time() - t0))
+print("Best estimator found by grid search:")
+print(clf.best_estimator_)
+
+# pdb.set_trace()
+###############################################################################
+# Quantitative evaluation of the model quality on the test set
+
+print("Predicting people's names on the test set")
+t0 = time()
+y_pred = clf.predict(X_test_pca)
+print("done in %0.3fs" % (time() - t0))
+
+print(classification_report(y_test, y_pred, target_names=target_names))
+print(confusion_matrix(y_test, y_pred, labels=range(n_classes)))
+
+
+###############################################################################
+# Qualitative evaluation of the predictions using matplotlib
+
+def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
+    """Helper function to plot a gallery of portraits"""
+    plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
+    plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
+    for i in range(n_row * n_col):
+        plt.subplot(n_row, n_col, i + 1)
+        plt.imshow(images[i].reshape((h, w)), cmap=plt.cm.gray)
+        plt.title(titles[i], size=12)
+        plt.xticks(())
+        plt.yticks(())
+
+
+# plot the result of the prediction on a portion of the test set
+
+def title(y_pred, y_test, target_names, i):
+    pred_name = target_names[y_pred[i]].rsplit(' ', 1)[-1]
+    true_name = target_names[y_test[i]].rsplit(' ', 1)[-1]
+    return 'predicted: %s\ntrue:      %s' % (pred_name, true_name)
+
+prediction_titles = [title(y_pred, y_test, target_names, i)
+                     for i in range(y_pred.shape[0])]
+
+plot_gallery(X_test, prediction_titles, h, w)
+
+# plot the gallery of the most significative eigenfaces
+eigenface_titles = ["eigenface %d" % i for i in range(eigenfaces.shape[0])]
+plot_gallery(eigenfaces, eigenface_titles, h, w)
+
+plt.show()
